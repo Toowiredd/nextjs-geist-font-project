f0198240149cbc138f4f799d45b3dd0497233f21
Toowiredd <lewistys420@gmail.com>
2025-05-25 21:13:18 -0700

    Add .next/static/chunks/src_app_dashboard_page_tsx_f94ed9f5._.js 1748232796082

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_f94ed9f5._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_three_build_three_module_7d17ea5c.js",
+  "static/chunks/node_modules_react-reconciler_25c8ec02._.js",
+  "static/chunks/node_modules_@react-three_fiber_dist_dc1c2c82._.js",
+  "static/chunks/node_modules_63f26863._.js",
+  "static/chunks/src_196d3b0e._.js"
+],
+    source: "dynamic"
+});
